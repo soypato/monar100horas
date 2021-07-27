ed2e1033be8348fcc31494f2774edf1e31d0ddcd
soypato <patriciotubio@gmail.com>
2021-07-27 18:24:14 -0300

    renamed "clock" for "timer", created "crono" and modified times after Jul 28 10:00:00 GMT-0300, fix font-size in h1/h2, and add "EN VIVO"

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+// Código adaptado de EDTeam en https://codepen.io/AlvaroFelipe/pen/EvJrOW
+
+// Cronómetro (crono)
+function getRemainingTimer(deadline) {
+    let now = new Date(),
+        remainTime = (now - new Date(deadline) + 1000) / 1000,
+        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
+        remainDays = Math.floor(remainTime / (3600 * 24));
+
+    return {
+        remainSeconds,
+        remainMinutes,
+        remainHours,
+        remainDays,
+        remainTime
+    }
+};
+
+const timer = (deadline, elem) => {
+    const el = document.getElementById(elem);
+
+    const timerUpdate = setInterval(() => {
+        let t = getRemainingTimer(deadline);
+        el.innerHTML = `${t.remainDays}d ${t.remainHours}h ${t.remainMinutes}m ${t.remainSeconds}s`;
+
+
+    }, 1000)
+};
+
+
+timer('Jul 28 2021 10:00:00 GMT-0300', 'crono');
+
+// Temporizador (timer)
 function getRemainingTime(deadline) {
     let now = new Date(),
         remainTime = (new Date(deadline) - now + 1000) / 1000,
@@ -30,6 +65,4 @@ const countdown = (deadline, elem, finalMessage) => {
     }, 1000)
 };
 
-countdown('Jul 28 2021 10:00:00 GMT-0300', 'clock', '¡Estamos en el 100 horas!');
-
-// Código adaptado de EDTeam en https://codepen.io/AlvaroFelipe/pen/EvJrOW
+countdown('Aug 1 2021 12:00:00 GMT-0300', 'timer', '¡El 100 horas ha concluído!');
