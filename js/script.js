8d0eaf18540f2baed57ede6af8ffe197829c1ae5
Patricio Tubio <77248407+soypato@users.noreply.github.com>
2021-10-24 02:36:40 -0300

    Add alert with SweetAlert Add a new framework from CDN and configure alert for index

--- js/script.js
+++ js/script.js
@@ -2,69 +2,68 @@
 
 // Cronómetro (crono)
 function getRemainingTimer(deadline) {
-    let now = new Date(),
-        remainTime = (now - new Date(deadline) + 1000) / 1000,
-        timerTime = (new Date(deadline) - now + 1000) / 1000,
+  let now = new Date(),
+    remainTime = (now - new Date(deadline) + 1000) / 1000,
+    timerTime = (new Date(deadline) - now + 1000) / 1000,
+    remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2),
+    remainMinutes = ("0" + Math.floor((remainTime / 60) % 60)).slice(-2),
+    remainHours = ("0" + Math.floor((remainTime / 3600) % 24)).slice(-2),
+    remainDays = Math.floor(remainTime / (3600 * 24));
 
-        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
-        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
-        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
-        remainDays = Math.floor(remainTime / (3600 * 24));
-
-    return {
-        remainSeconds,
-        remainMinutes,
-        remainHours,
-        remainDays,
-        remainTime
-    }
-};
+  return {
+    remainSeconds,
+    remainMinutes,
+    remainHours,
+    remainDays,
+    remainTime
+  };
+}
 
 const timer = (deadline, elem) => {
-    const el = document.getElementById(elem);
+  const el = document.getElementById(elem);
 
-    const timerUpdate = setInterval(() => {
-        let t = getRemainingTimer(deadline);
-        el.innerHTML = `${t.remainDays}d ${t.remainHours}h ${t.remainMinutes}m ${t.remainSeconds}s`;
-
-
-    }, 1000)
+  const timerUpdate = setInterval(() => {
+    let t = getRemainingTimer(deadline);
+    el.innerHTML = `${t.remainDays}d ${t.remainHours}h ${t.remainMinutes}m ${t.remainSeconds}s`;
+  }, 1000);
 };
 
-
-timer('Jul 28 2021 10:00:00 GMT-0300', 'crono');
+timer("Jul 28 2021 10:00:00 GMT-0300", "crono");
 
 // Temporizador (timer)
 function getRemainingTime(deadline) {
-    let now = new Date(),
-        remainTime = (new Date(deadline) - now + 1000) / 1000,
-        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
-        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
-        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
-        remainDays = Math.floor(remainTime / (3600 * 24));
+  let now = new Date(),
+    remainTime = (new Date(deadline) - now + 1000) / 1000,
+    remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2),
+    remainMinutes = ("0" + Math.floor((remainTime / 60) % 60)).slice(-2),
+    remainHours = ("0" + Math.floor((remainTime / 3600) % 24)).slice(-2),
+    remainDays = Math.floor(remainTime / (3600 * 24));
 
-    return {
-        remainSeconds,
-        remainMinutes,
-        remainHours,
-        remainDays,
-        remainTime
-    }
-};
+  return {
+    remainSeconds,
+    remainMinutes,
+    remainHours,
+    remainDays,
+    remainTime
+  };
+}
 
 const countdown = (deadline, elem, finalMessage) => {
-    const el = document.getElementById(elem);
+  const el = document.getElementById(elem);
 
-    const timerUpdate = setInterval(() => {
-        let t = getRemainingTime(deadline);
-        el.innerHTML = `${t.remainDays}d ${t.remainHours}h ${t.remainMinutes}m ${t.remainSeconds}s`;
+  const timerUpdate = setInterval(() => {
+    let t = getRemainingTime(deadline);
+    el.innerHTML = `${t.remainDays}d ${t.remainHours}h ${t.remainMinutes}m ${t.remainSeconds}s`;
 
-        if (t.remainTime <= 1) {
-            clearInterval(timerUpdate);
-            el.innerHTML = finalMessage;
-        }
-
-    }, 1000)
+    if (t.remainTime <= 1) {
+      clearInterval(timerUpdate);
+      el.innerHTML = finalMessage;
+    }
+  }, 1000);
 };
 
-countdown('Aug 1 2021 14:00:00 GMT-0300', 'timer', '¡El 100 horas ha concluído!');
+countdown(
+  "Aug 1 2021 14:00:00 GMT-0300",
+  "timer",
+  "¡El 100 horas ha concluído!"
+);
---
+++ js/swal.js
@@ -0,0 +1,5 @@
+swal(
+  "¡Antes de continuar!",
+  `Este proyecto fue creado con el objetivo en un proyecto que ya finalizó, por lo que puede que veas algunas zonas modificadas en el tiempo, o recursos que fueron eliminados de servidores que no dependen de nosotros ¡gracias!`,
+  "warning"
+);
